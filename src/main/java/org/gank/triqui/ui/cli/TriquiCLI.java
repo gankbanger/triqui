966f78d82fcea8baa9d90b0b8b6ad3667882a90d
gank <abelardovacca@yahoo.com>
2021-01-31 10:47:27 -0500

    CLI implementation

---
+++ src/main/java/org/gank/triqui/ui/cli/TriquiCLI.java
@@ -0,0 +1,74 @@
+package org.gank.triqui.ui.cli;
+
+import java.util.Scanner;
+
+import org.gank.triqui.juego.Casilla;
+import org.gank.triqui.juego.SuscriptorAlJuego;
+import org.gank.triqui.juego.Triqui;
+
+public class TriquiCLI implements SuscriptorAlJuego {
+    public static void main(String[] args) {
+        TriquiCLI cli = new TriquiCLI();
+        Triqui juego = new Triqui();
+        juego.inscribirseNotificacionesTurnos(cli);
+        boolean continuar = true;
+
+        try (Scanner in = new Scanner(System.in)) {
+            imprimirTablero(juego);
+            while (!juego.finJuego() && continuar) {
+                jugarTurno(juego, in);
+                imprimirTablero(juego);
+                if (juego.finJuego()) {
+                    continuar = continuarJugando(in);
+                    if (continuar) {
+                        juego.nuevoJuego();
+                        imprimirTablero(juego);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void jugarTurno(Triqui juego, Scanner in) {
+        String resp = "";
+
+        in.useDelimiter(System.lineSeparator());
+        do {
+            System.out.println("Escoger posición dónde marcar: [x] [y]");
+            resp = in.next();
+        } while (!resp.matches("[1-3] [1-3]"));
+
+        String[] coord = resp.split(" ");
+        juego.marcar(Integer.valueOf(coord[0]) - 1, Integer.valueOf(coord[1]) - 1);
+    }
+
+    private static void imprimirTablero(Triqui juego) {
+        for (int i = 0; i < Triqui.DIMENSION_TABLERO; i++) {
+            for (int j = 0; j < Triqui.DIMENSION_TABLERO; j++) {
+                String casilla = juego.obtenerCasilla(i, j).toString();
+                casilla = "VACIA".equals(casilla) ? "_" : casilla;
+                System.out.print(casilla + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    private static boolean continuarJugando(Scanner in) {
+        String resp = null;
+        while (!"S".equals(resp) && !"N".equals(resp)) {
+            System.out.println("¿Jugar de nuevo? [S]í / [N]o");
+            resp = in.next();
+        }
+        return "S".equals(resp.trim());
+
+    }
+
+    @Override
+    public void victoria(Casilla ganador, int[][] lineaGanadora) {
+        System.out.println(String.format("%s ganó el juego con la linea (%d, %d), (%d,%d), (%d,%d)", ganador.toString(),
+                lineaGanadora[0][0], lineaGanadora[0][1], lineaGanadora[1][0], lineaGanadora[1][1], lineaGanadora[2][0],
+                lineaGanadora[2][1]));
+        System.out.println("");
+    }
+}
